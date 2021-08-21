404eb6868143e8231a6aa6d3d852b6396f02e9e5
Michael Milewski <saramic@gmail.com>
2021-08-21 10:29:31 +1000

    QR code generation and image

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'sinatra'
 
+# QR code generation
+gem "rqrcode", "~> 2.0"
+
 group :development do
   gem 'pry'
   gem 'rubocop'
--- app.rb
+++ app.rb
@@ -2,9 +2,20 @@
 
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'rqrcode'
 
 set :root, File.dirname(__FILE__)
 
 get '/' do
+  qrcode = RQRCode::QRCode.new("https://kaha-govhack-2021.herokuapp.com/")
+  qrcode = RQRCode::QRCode.new("https://bit.ly/kahaGH2021")
+  @qr_svg = qrcode.as_svg(
+    color: "212121",
+    shape_rendering: "crispEdges",
+    module_size: 11,
+    standalone: true,
+    use_path: true,
+    viewbox: "0 0 500 500"
+  )
   erb :index
 end
